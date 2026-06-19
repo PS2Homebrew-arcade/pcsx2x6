1bd84786347ead50eb7c772c7dfa741122d16f17
VooDooUdo2 <chad.okray@gmail.com>
2026-06-18 19:44:53 -0700

    fix:  cross-platform compliation support

--- pcsx2/MameHookerProxy.h
+++ pcsx2/MameHookerProxy.h
@@ -1,15 +1,25 @@
 #pragma once
+#include <string>
+#ifdef _WIN32
 #include <windows.h>
+#endif
 #include <thread>
 #include <chrono>
+#include "common/Console.h"
 
 class MameHookerProxy
 {
 public:
   std::string activeGame = "";
+#ifdef _WIN32
   HANDLE hPipe = nullptr;
   HANDLE hPipeGunA = nullptr;
   HANDLE hPipeGunB = nullptr;
+#else
+  void* hPipe = nullptr;
+  void* hPipeGunA = nullptr;
+  void* hPipeGunB = nullptr;
+#endif
 
   bool pipeConnected = false;
   bool pipeConnectedGunA = false;
@@ -17,12 +27,20 @@ public:
 
   bool active = false;
 
+#ifdef _WIN32
   PROCESS_INFORMATION* processInfo = nullptr;
+#else
+  void* processInfo = nullptr;
+#endif
 
 
   static MameHookerProxy& GetInstance();
   static bool fileExists(const wchar_t* filePath);
+#ifdef _WIN32
   static void launchProgram(const char* programPath, PROCESS_INFORMATION& processInfo);
+#else
+  static void launchProgram(const char* programPath, void* processInfo);
+#endif
   static std::string getExecutableDirectory();
   void Init();
   void CloseGame();
@@ -30,3 +48,15 @@ public:
   void SendState(std::string key, int value);
   void StartGame(std::string id);
 };
+
+#ifndef _WIN32
+inline MameHookerProxy& MameHookerProxy::GetInstance() { static MameHookerProxy s; return s; }
+inline bool MameHookerProxy::fileExists(const wchar_t*) { return false; }
+inline void MameHookerProxy::launchProgram(const char*, void*) {}
+inline std::string MameHookerProxy::getExecutableDirectory() { return std::string(); }
+inline void MameHookerProxy::Init() {}
+inline void MameHookerProxy::CloseGame() {}
+inline void MameHookerProxy::Gunshot(int) {}
+inline void MameHookerProxy::SendState(std::string, int) {}
+inline void MameHookerProxy::StartGame(std::string id) { Console.Warning("MameHookerProxy: StartGame called on non-Windows platform, currently MameHooker support is Windows only"; }
+#endif
